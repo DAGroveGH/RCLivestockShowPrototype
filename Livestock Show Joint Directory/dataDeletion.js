9cb03f1f62ad25a4ee0b7f3274446c157e85c9cc
DAGroveGH <65041684+DAGroveGH@users.noreply.github.com>
2020-05-12 17:50:31 -0400

    Update dataDeletion.js

--- Livestock Show Joint Directory/dataDeletion.js
+++ Livestock Show Joint Directory/dataDeletion.js
@@ -1,14 +1,45 @@
    function changeData() {
-                var t = document.getElementById("tb").value;
-                var fm = document.createElement("FORM");
-                fm.setAttribute(“id”, “editTb”);
-                fm.setAttribute(“action”, “dataUpdate.php”);
-                fm.setAttribute(“method”, “POST”);
-                document.main.appendChild(fm);
-                var pt = document.createElement("INPUT");
-                pt.setAttribute(“type”, “text”);
-                pt.setAttribute(“name”, t);
-                pt.setAttribute(“value”, t);
-                document.getElementById("editTb").appendChild(pt);
-                document.getElementById("editTb").submit();
+       var t = document.getElementById("tb").value;
+       switch(t) {
+            case "Exhibitor":
+                document.getElementById("etb").setAttribute("name", "Exhibitor");
+                document.getElementById("etb").setAttribute("value", "Exhibitor");
+                document.getElementById("con1").setAttribute("name", "Exhibitor_Lname");
+                document.getElementById("con1").setAttribute("value", "Exhibitor Last Name");
+                document.getElementById("con2").setAttribute("name", "Exhibitor_Fname");
+                document.getElementById("con2").setAttribute("value", "Exhibitor Fast Name");
+                document.getElementById("con3").setAttribute("name", "Phone_Number");
+                document.getElementById("con3").setAttribute("type", "tel");
+                document.getElementById("con3").setAttribute (pattern, "[0-9]{3}-[0-9]{3}-[0-9]{4}");
+                document.getElementById("con3").setAttribute("value", "Exhibitor Phone");
+                break;
+            case "Animal":
+                document.getElementById("etb").setAttribute("value", "Animal");
+                document.getElementById("con1").setAttribute("name", "Exhibitor_Num");
+                document.getElementById("con1").setAttribute("value", "Exhibitor Number");
+                document.getElementById("con2").setAttribute("name", "Breed_Num");
+                document.getElementById("con2").setAttribute("value", "Breed Number");
+                document.getElementById("con3").setAttribute("name", "Breed_Letter");
+                document.getElementById("con3").setAttribute("value", "Breed Letter");
+                break;
+            case "Show":
+                document.getElementById("etb").setAttribute("name", "Show");
+                document.getElementById("etb").setAttribute("value", "Show");
+                document.getElementById("con1").setAttribute("name", "Show_Name");
+                document.getElementById("con1").setAttribute("value", "Show Name");
+                document.getElementById("con2").setAttribute("name", "Session_Num");
+                document.getElementById("con2").setAttribute("value", "Session Number");
+                document.getElementById("con3").setAttribute("name", "Show_Year");
+                document.getElementById("con3").setAttribute("value", "Show Year");
+                break;
+            case "Animal_Category":
+                document.getElementById("etb").setAttribute("name", "Animal_Category");
+                document.getElementById("etb").setAttribute("value", "Animal_Category");
+                document.getElementById("con1").setAttribute("name", "Breed_Num");
+                document.getElementById("con1").setAttribute("value", "Breed Number");
+                document.getElementById("con2").setAttribute("name", "Breed_Letter");
+                document.getElementById("con2").setAttribute("value", "Breed Letter");
+                document.getElementById("con3").setAttribute("name", "Breed_Name");
+                document.getElementById("con3").setAttribute("value", "Breed Name");   
+       }
    }
